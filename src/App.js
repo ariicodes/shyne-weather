9ba14533f80aa0a5563094ef8782cbc864b90767
ariicodes <arielaisrael12@gmail.com>
2022-08-28 02:26:06 -0400

    second commit, axios added and readme changed

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+import axios from 'axios'
 import './App.css'
 import AdditionalInfo from './components/additionalInfo'
 import Alert from './components/alert'
@@ -5,13 +6,19 @@ import Forecast from './components/forecast'
 import LocationSearch from './components/locationSearch'
 import TempInfo from './components/tempInfo'
 
-function App() {
+const App = () => {
 
-  // fetch(`https://api.weatherbit.io/v2.0/current?postal_code=20002&key=b26a6db85afe4b3e8971b1f14016ae3c&units=I&include=minutely`)
-  // .then( res => res.json() )
-  // .then( data => {
-    
-  // })
+  const currentUrl = `https://api.weatherbit.io/v2.0/current?postal_code=20002&key=b26a6db85afe4b3e8971b1f14016ae3c&units=I&include=minutely`
+
+  const currentData = () => {
+    axios.get(currentUrl)
+    .then((res) => {
+      console.log('RESPONSE!: ', res.data.data[0])
+    })
+    .catch((e) => {
+      console.log('ERROR!: ', e)
+    })
+  }
 
   return (
     <div className="App flex flex-col justify-center gap-2 h-screen bg-gradient-to-b from-slate-900 to-slate-800 text-stone-300 p-5 text-sm font-open">
